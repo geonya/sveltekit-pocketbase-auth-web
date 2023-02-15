cce7a146e5fc3480750031a11519e7bd0c8937d4
geonya <geony8410@gmail.com>
2023-02-15 09:50:35 +0900

    create new project page setup

---
+++ src/routes/projects/new/+page.server.ts
@@ -0,0 +1,26 @@
+import { error, redirect } from "@sveltejs/kit";
+import type { Actions, PageServerLoad } from "./$types";
+
+export const load: PageServerLoad = async ({ locals }) => {
+  if (!locals.pb.authStore.isValid) {
+    throw redirect(303, "/login");
+  }
+};
+
+export const actions: Actions = {
+  create: async ({ locals, request }) => {
+    const formData = await request.formData();
+    const thumbnail = formData.get("thumbnail") as File;
+    if (thumbnail.size === 0) {
+      formData.delete("thumbnail");
+    }
+    formData.append("user", locals.user.id);
+    try {
+      await locals.pb.collection("projects").create(formData);
+    } catch (err: any) {
+      console.error(err);
+      throw error(err.status, err.message);
+    }
+    throw redirect(303, "/");
+  },
+};
---
+++ src/routes/projects/new/+page.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+  import Input from "$lib/components/Input.svelte";
+</script>
+
+<div class="flex h-full w-full flex-col p-2">
+  <div class="w-full">
+    <form
+      method="post"
+      action="?/create"
+      class="flex w-full flex-col items-center space-y-2"
+      enctype="multipart/form-data"
+    >
+      <h3 class="text-3xl font-bold ">Tell us about your project.</h3>
+      <p class="mt-2 text-lg">
+        We'll need the name, tagline, link, and description
+      </p>
+      <Input id="name" label="Project Name" />
+      <Input id="tagline" label="Project Tagline" />
+      <Input id="url" label="Project URL" />
+      <div class="form-control w-full max-w-lg">
+        <label for="description" class="label pb-1 font-medium">
+          <span class="label-text">Project Description</span>
+        </label>
+        <textarea
+          name="description"
+          class="textarea-bordered textarea h-24 resize-none"
+        />
+      </div>
+      <div class="form-control w-full max-w-lg">
+        <label for="thumnail">
+          <span class="label-text">Project Thumbnail</span>
+        </label>
+        <input
+          type="file"
+          name="thumbnail"
+          id="thumbnail"
+          class="file-input-bordered file-input w-full max-w-lg"
+        />
+      </div>
+      <div class="w-full max-w-lg pt-3">
+        <button class="btn-primary btn w-full max-w-lg">Create</button>
+      </div>
+    </form>
+  </div>
+</div>
